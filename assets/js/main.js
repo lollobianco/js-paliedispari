e0cc2fa095ed0bd0bbcad03b2e9652bcdad8f8ae
lollobianco <loreb2002@gmail.com>
2022-10-27 15:05:22 +0200

    Primo snack completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
+let word = prompt('inserisci una parola per scoprire se è palindroma oppure no: ');
+
+
+function palindroma(){
+
+   let reversedWord = '';
+
+   for (let i = word.length-1; i >= 0; i--){
+
+      reversedWord = reversedWord += word[i];
+
+   }
+
+   return reversedWord;
+
+}
+
+const reversedWord = palindroma();
+
+console.log(`La parola "${word}" al contrario e: ${reversedWord}`);
+
+if (reversedWord == word){
+
+   alert(`La parola "${word}" è palindroma: ${word} --> ${reversedWord}`);
+   
+}  else{
+
+      alert(`La parola "${word}" non è palindroma: ${word} --> ${reversedWord}`);
+
+}
 
